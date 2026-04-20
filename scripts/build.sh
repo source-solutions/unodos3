3e098dd0daf5811ed608775852f6d863d8be89b6
aowendev <aowen@translationcommons.org>
2026-04-20 19:10:14 +0100

    clean 3-12 branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
-cp rasm/rasm-deb-arm64 src/rasm
 cd src
-./rasm -pasmo kernel.asm -ob ../bin/kernel.bin -sz -os ../bin/symbols.txt
+rasm -pasmo kernel.asm -ob ../bin/kernel.bin -sz -os ../bin/symbols.txt
 cd ../bin
 mv unodos0.sys unodos.sys
 cat unodos1.sys >> unodos.sys
